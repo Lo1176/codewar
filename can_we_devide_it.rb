ed18414956459d1694382b488e28bbd7c8497533
Laurent Binder <laurentbinder@gmail.com>
2020-10-01 18:44:19 +0200

    refacto

--- can_we_devide_it.rb
+++ can_we_devide_it.rb
@@ -1,16 +1,19 @@
-# Your task is to create functionisDivideBy (or is_divide_by) to check if an integer number is divisible by each out of two arguments.
+# Your task is to create functionisDivideBy (or is_divide_by)
+# to check if an integer number is divisible by each out of two arguments.
 
-def is_devide_by(number, a, b)
-  number % a == 0 && number % b == 0
+def devide_by?(number, a, b)
+  # number % a == 0 && number % b == 0
+  (number % a).zero? && (number % b).zero?
 end
 
 # with .all?
-def is_devide_by_all(number, *args)
+def devide_by_all?(number, *args)
   args.all? { |divisor| number % divisor == 0 }
 end
 
-puts "methode with &&"
-puts is_devide_by(-12, 2, -5)
-puts "--------------"
-puts "methode with all?"
-puts is_devide_by_all(-12, 2, -5)
+# test
+puts 'methode with &&'
+puts devide_by?(-12, 2, -5)
+puts '--------------'
+puts 'methode with all?'
+puts devide_by_all?(-12, 2, -5)
